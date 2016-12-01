edb709a9e26056b538ed0255f36d00bd554899f1
raul vega <raul vega>
2016-12-01 11:15:23 -0600

    homework 6-working-one issue to resolve

--- assets/javascript/giphyjs.js
+++ assets/javascript/giphyjs.js
@@ -32,80 +32,87 @@ $('#moviesView').on('click', '.movie', function() {
     var title = $(this).attr('data-name');
 
 
-
-//Please note that I am only bringin back 1 gif.  i shall change to the required 10 once the program is working.
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + title + "&limit=1&api_key=dc6zaTOxFJmzC";
+    //The queryURL and ajax to get the data from giphy. 
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + title + "&limit=10&api_key=dc6zaTOxFJmzC";
 
     $.ajax({ url: queryURL, method: 'GET' })
         .done(function(response) {
-            
+
+            $("#gifView").empty();
             //console.log(response);
 
+            //var movieDiv = $("<div>");
             //console.log("this is array length: " + response.data.length);
 
+            //For loop to get 10 gif's from the giphy website
             for (i = 0; i < response.data.length; i++) {
 
-                //console.log("the i counter: " + i);
-                
-//This console log shows me the rating and the url for the still and animated gif.               
+                console.log("the i counter: " + i);
+                //console.log("response data length: " + response.data.length);
+
+                //This console log shows me the rating and the url for the still and animated gif.               
                 console.log("this is the rating: " + response.data[i].rating);
                 console.log("this is the still URL: " + response.data[i].images.fixed_height_small_still.url);
                 console.log("this is the action URL: " + response.data[i].images.fixed_height_small.url);
 
 
-//This variable was working this morning.  I was able to make the still and animated gifs appear on the screen.
-
-                //var stillgiphyImage = "<img src= " + response.data[i].images.fixed_height_small_still.url + ">";
-                //var movinggiphyImage = "<img src= " + response.data[i].images.fixed_height_small.url + ">";
-                
-
-
-                //var movinggiphyImage = "<img src= " + response.data[i].images.fixed_height_small.url + ">";
                 //This section contains the variable for the image that is on the screen.  I am setting the attributes
                 //of the variable so that I can use in the next section (animate/still).
-
-                //var stillgiphyImage = "<img src= " + response.data[i].images.fixed_height_small_still.url + 
-                //"data-still" = response.data[i].images.fixed_height_small_still.url,
-                //"data-animate" = response.data[i].images.fixed_height_small.url,
-                //"data-state" = "still",
-                //class = "gif" ">";
-
-//This is what I am trying to work on at the moment.  it is not working and the image does not appear on the screen.
-  
-                var movinggiphyImage = $("<img>");
-                movinggiphyImage.addClass("movinggiphyImage");
-                movinggiphyImage.attr({
+     
+                var giphyImage = $("<img>");
+                giphyImage.addClass("movinggiphyImage");
+                giphyImage.attr({
                     "src": response.data[i].images.fixed_height_small_still.url,
                     "data-still": response.data[i].images.fixed_height_small_still.url,
                     "data-animate": response.data[i].images.fixed_height_small.url,
                     "data-state": "still",
+                    "class": "gif",
                 });
-  
-                
-//This does not appear to work anymore either. nothing appears on the screen when the animal buttons are pressed.
 
-                //This section will make information appear in the screen.
-                //$('#movieInfo').text(JSON.stringify(data));
-                var movieDiv = $("<div>");
-                //movieDiv.append("<h2>Title " + data.title + "<h2>");
-                movieDiv.append("<p>Rating " + response.data[i].rating + "<p>");
-                movieDiv.append("<p>Moving Image " + movinggiphyImage + "<p>");
-                //movieDiv.append("<p>Still Image " + image + "<p>");
-                movieDiv.append("<p>Moving Image " + stillgiphyImage + "<p>");
-                
+                //Appending the rating and images to the page
+                $("#gifRating").append("Rating: " + response.data[i].rating + " ");
+                $("#gifView").append(giphyImage);
 
-         
+
+                //This section contains the code for still and animate images
+                $(".gif").on("click", function() {
+                    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+                    var state = $(this).attr("data-state");
+
+                    if (state === "still") {
+                        $(this).attr("src", $(this).data("animate"));
+                        $(this).attr("data-state", "animate");
+                    } else {
+                        // If the clicked image's state is still, update it's src attribute to what it's data-animate value is.
+                        // Then set the image's data-state to animate
+                        $(this).attr("src", $(this).data("still"));
+                        $(this).attr("data-state", "still");
+                    }
+                });
+
+                //Old stuff I have not deleted yet.
+                        //This section will make information appear in the screen.
+                        //$('#movieInfo').text(JSON.stringify(data));
+
+                        //movieDiv.append("<h2>Title " + data.title + "<h2>");
+                        //movieDiv.append("<p>Rating " + response.data[i].rating + "<p>");
+                        //movieDiv.append("<p>Moving Image<p>"); 
+                        //movieDiv.append(movinggiphyImage[0]);
+                        //movieDiv.append("<p>Still Image " + image + "<p>");
+                        //movieDiv.append("<p>Moving Image " + stillgiphyImage[i] + "<p>");
+                        //$('body').append(movieDiv);
+
+
             }
 
         });
 });
 
- 
 
 
 // ========================================================
 
-// This function handles events where one button is clicked
+// This function handles events where one button is clicked.  We are adding buttons
 $('#addMovie').on('click', function() {
 
     // This line of code will grab the input from the textbox
@@ -122,7 +129,7 @@ $('#addMovie').on('click', function() {
 });
 
 // ========================================================
-                
+
 
 
 
